8c001ff4b0076383ca023686cd033234efd709d7
avi2413 <63181427+avi2413@users.noreply.github.com>
2020-09-05 15:03:30 +1000

    Add files via upload

---
+++ build/precache-manifest.317b27bff9d8a6096830be25c383134f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de7400bbe3615812ac80e601ba80edcd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2e1624960486a03e4c2c",
+    "url": "/static/css/main.050523cc.chunk.css"
+  },
+  {
+    "revision": "3027c0ae185897c17bab",
+    "url": "/static/js/2.8ced723a.chunk.js"
+  },
+  {
+    "revision": "2a65342a7cef87ae756f1549b040326b",
+    "url": "/static/js/2.8ced723a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e1624960486a03e4c2c",
+    "url": "/static/js/main.73542e07.chunk.js"
+  },
+  {
+    "revision": "9db5ea5ae5c02832f52d",
+    "url": "/static/js/runtime-main.a6d80b84.js"
+  }
+]);
